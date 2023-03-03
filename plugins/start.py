4d3260cba9af3e71ca4f31727978f000fadeefaa
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-03-03 23:49:49 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -45,8 +45,8 @@ async def start(client, message):
     try:
         id = message.text.split(' ')[1]
     except:
-        pesan=f'Halo {waktu} {message.from_user.first_name}❤\n\nGW ADALAH **MS VIDEO EDITOR💻**\n\nGw bisa ngedit video lu kaya **UBAH NAMA,ENCODE RESOLUSI,POTONG DURASI VIDEO,EXTRAC VIDEO KE AUDIO MP3,SPESIAL CUSTOM THUMBNAIL**' 
-	await message.reply_photo(photo=LAZY_PIC,
+           pesan=f'Halo {waktu} {message.from_user.first_name}❤\n\nGW ADALAH **MS VIDEO EDITOR💻**\n\nGw bisa ngedit video lu kaya **UBAH NAMA,ENCODE RESOLUSI,POTONG DURASI VIDEO,EXTRAC VIDEO KE AUDIO MP3,SPESIAL CUSTOM THUMBNAIL**' 
+	   await message.reply_photo(photo=LAZY_PIC,
                                 caption=pesan,
                                 reply_markup=InlineKeyboardMarkup([
                                       [InlineKeyboardButton("DEVELOPER👤", url="https://t.me/MSDZULQURNAIN")],
@@ -71,7 +71,7 @@ async def start(client, message):
 				      [InlineKeyboardButton("TUTORIAL", callback_data='tutor')]
                                       ]))
             except:
-                return
+                   return
         else:
             await client.send_message(id, "Selamat tod lu menang 3GB Upload limit")
             _user_ = find_one(int(id))

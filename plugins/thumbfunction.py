c0fc662c8d305c69a74f99e1677d44cc4942033a
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-03-11 16:25:11 +0700

    Add files via upload

---
+++ plugins/thumbfunction.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters
+from helper.database import find, delthumb, addthumb
+
+@Client.on_message(filters.private & filters.command(['lihatthumbnail']))
+async def lihatthumbnail(client,message):
+		print(message.chat.id)
+		thumb = find(int(message.chat.id))[0]
+		if thumb :
+			await client.send_photo(message.chat.id,photo =f"{thumb}")
+		else:
+			await message.reply_text("**Lu kaga punya thumbnail kocak😒**\n\nklik /pasangthumbnail buat pasang thumbnail😑")
+	
+	 
+@Client.on_message(filters.private & filters.command(['hapusthumbnail']))
+async def hapusthumbnail(client,message):
+	delthumb(int(message.chat.id))
+	await message.reply_text("**Thumbnail berhasil dihapus🗑**")
+	
+@Client.on_message(filters.private & filters.command(['pasangthumbnail']))
+async def pasangthumbnail(client,message):
+        await message.reply_text("**KIRIM FOTO LU BUAT THUMBNAIL**")
+
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client,message):
+	file_id = str(message.photo.file_id)
+	addthumb(message.chat.id , file_id)
+	await message.reply_text("**Thumbnail berhasil disimpan**✅\n\nSilahkan kirim video dan pilih ubah nama/rename buat menerapkan thumbnail ini😘\n\nUntuk mengganti thumbnail hapus dulu thumbnail nya klik /hapusthumbnail trs pasang lagi klik /pasangthumbnail gituuuuu🔥")

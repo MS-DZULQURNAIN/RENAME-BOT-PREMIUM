72d389a1fcf498705ddd588dbc470144d829d7b1
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-03-11 18:19:40 +0700

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-@Client.on_message(filters.private & filters.command(["refer"]))
+@Client.on_message(filters.private & filters.command(["referral"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[[ InlineKeyboardButton("Share Your Link" ,url=f"https://t.me/share/url?url=https://t.me/tlgrenamerbot?start={message.from_user.id}") ]   ])
-    await message.reply_text(f"Refer And Earn Get 3GB Upload Limit\nPer Refer 3GB\n Your Link :- https://t.me/tlgrenamerbot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
+       		[[ InlineKeyboardButton("Bagikan link mu" ,url=f"https://t.me/share/url?url=https://t.me/MsRenameBot?start={message.from_user.id}") ]   ])
+    await message.reply_text(f"Bagikan referral lu ketemen lu ntar lu dapet 3GB Limit Upload\nPer Referral 3GB\n Link lu : https://t.me/MsRenameBot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
     

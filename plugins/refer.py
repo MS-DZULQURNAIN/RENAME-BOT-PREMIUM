e0a9a340f6c0e2b4a029f1ef06a334b116ed6576
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-03-12 22:23:14 +0700

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 @Client.on_message(filters.private & filters.command(["referral"]))
-async def refer(client,message):
+async def referral(client,message):
     reply_markup = InlineKeyboardMarkup(
        		[[ InlineKeyboardButton("Bagikan link mu" ,url=f"https://t.me/share/url?url=https://t.me/MsRenameBot?start={message.from_user.id}") ]   ])
     await message.reply_text(f"Bagikan referral lu ketemen lu ntar lu dapet 3GB Limit Upload\nPer Referral 3GB\n Link lu : https://t.me/MsRenameBot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
